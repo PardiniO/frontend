9b976ecb7e8057231c33bdd87444643cc39e929a
PardiniO <pardinioriana@gmail.com>
2025-10-28 20:32:05 -0300

    Creación de guard, modificación del auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,63 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
-import { IUser } from '../interfaces/user';
-import { IAuthResponse } from "../interfaces/auth";
-import { StorageService } from './storage.service';
-import {  } from "../../environments/";
+import { environment } from "../../environments/environment";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class IAuthService {
-  private apiUrl = `${environment.apiUrl}/auth`;
-  private userSubject = new BehaviorSubject<IUser | null>(null);
-  user$ = this.userSubject.asObservable();
-
-  constructor(
-    private http: HttpClient,
-    private storage: StorageService
-  ) { 
-    const token = this.storage.getToken();
-    if (token) this.getProfile().subscribe();
-  }
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private apiUrl = environment.apiUrl;
+  
+  constructor(private http: HttpClient) { }
 
   login(data: { email: string; password: string }) {
-    return this.http.post<IAuthResponse>(`${this.apiUrl}/login`, data).pipe(
-      tap(res => {
-        this.storage.setToken(res.token);
-        if (res.refreshToken) {
-          this.storage.setRefresh(res.refreshToken);
-        }
-        this.userSubject.next(res.user);
-      })
-    );
+    return this.http.post(`${this.apiUrl}/auth/login`, data);
   }
 
   register(data: { username: string; email: string; password: string }) {
-    return this.http.post<IAuthResponse>(`${this.apiUrl}/register`, data).pipe(
-      tap(res => {
-        this.storage.setToken(res.token);
-        if (res.refreshToken) {
-          this.storage.setRefresh(res.refreshToken);
-        }
-        this.userSubject.next(res.user);
-      })
-    );
+    return this.http.post(`${this.apiUrl}/auth/register`, data);
   }
 
   logout() {
-    this.storage.clearAll();
-    this.userSubject.next(null);
+    localStorage.removeItem('token');
   }
 
-  getProfile() {
-    return this.http.get<IUser>(`${this.apiUrl}/me`).pipe(
-      tap(user => this.userSubject.next(user))
-    );
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
   }
 
-  get currentUser(): IUser | null {
-    return this.userSubject.value;
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
   }
 }
