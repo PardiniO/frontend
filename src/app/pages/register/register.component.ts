bcba1ccd52277bf73b827d18b5f7293549e48d55
PardiniO <pardinioriana@gmail.com>
2025-10-28 22:04:10 -0300

    Creación componente: register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+    form!: FormGroup;
+    error!: '';
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private auth: AuthService,
+    private router: Router
+  ) {}
 
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+
+    const credentials = this.form.value as { username: string; email: string; password: string };
+
+    this.auth.login(credentials).subscribe({
+      next: (res) => {
+        this.auth.saveToken(res.token);
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        this.error = err.error?.message || 'Error al registrarse';
+      }
+    });
+  }
 }
