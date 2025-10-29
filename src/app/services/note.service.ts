b971c901d44c284a32055d8acb2bfc4de9deaf42
PardiniO <pardinioriana@gmail.com>
2025-10-29 19:17:44 -0300

    Modificación del componente reader

---
+++ src/app/services/note.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { Observable } from "rxjs";
+import { INote } from "../interfaces/note";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getNotes(bookId: number): Observable<INote[]> {
+    return this.http.get<INote[]>(`${this.apiUrl}/books/${bookId}/notes`);
+  }
+
+  addNotes(note: INote): Observable<INote> {
+    return this.http.post<INote>(`${this.apiUrl}/books${note.bookId}/notes`, note);
+  }
+
+  deleteNotes(bookId: number, noteId: number) {
+    return this.http.delete(`${this.apiUrl}/books/${bookId}/notes/${noteId}`);
+  }
+}
