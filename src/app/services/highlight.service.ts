0ae235a3a17d548d3582757aceb1e789fd77369b
PardiniO <pardinioriana@gmail.com>
2025-10-29 19:26:43 -0300

    Creación servicio highlight

---
+++ src/app/services/highlight.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
+import { IHighlight } from "../interfaces/highlight";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HighlightService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getHighlights(bookId: number): Observable<IHighlight[]> {
+    return this.http.get<IHighlight[]>(`${this.apiUrl}/books/${bookId}/highlights`);
+  }
+
+  addHighlights(highlight: IHighlight) {
+    return this.http.post<IHighlight>(`${this.apiUrl}/books/${highlight.bookId}/highlights`, highlight);
+  }
+
+  deleteHighlights(bookId: number, highlightId: number) {
+    return this.http.delete(`${this.apiUrl}/books/${bookId}/highlights/${highlightId}`);
+  }
+}
