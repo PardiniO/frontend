e33528a693dee37d995af2947636ddf6a4b4b01b
PardiniO <pardinioriana@gmail.com>
2025-10-31 21:26:14 -0300

    Creación servicio de biblioteca

---
+++ src/app/core/services/library.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { IReadingStatus } from "../../interfaces/reading";
+import { IFolder } from "../../interfaces/folder";
+import { IBook } from "../../interfaces/book";
+import { environment } from '../../../environments/environment';
+
+type filterStatus = IReadingStatus['status'];
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LibraryService {
+  private apiUrl = `${environment.apiUrl}/library`;
+
+  constructor(private http: HttpClient) {}
+
+  getAllUserBooks(): Observable<IBook[]> {
+    return this.http.get<IBook[]>(`${this.apiUrl}/books`);
+  }
+
+  getBooksByStatus(status: filterStatus): Observable<IBook[]> {
+    return this.http.get<IBook[]>(`${this.apiUrl}/books?status=${status}`);
+  }
+
+  getUserFolders(): Observable<IFolder[]> {
+    return this.http.get<IFolder[]>(`${this.apiUrl}/folders`);
+  }
+
+  createFolder(name: string): Observable<IFolder> {
+    const body = { name };
+    return this.http.post<IFolder>(`${this.apiUrl}/folders`, body);
+  }
+
+  addBookToFolder(folderId: number, bookId: number): Observable<IFolder> {
+    const body = { bookId };
+    return this.http.patch<IFolder>(`${this.apiUrl}/folders/${folderId}/add`, body);
+  }
+}
